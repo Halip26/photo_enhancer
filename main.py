a86f640ae8f6ee774295fe13cc671ccb92b2960f
Halip26 <halipuddin.angko@gmail.com>
2023-06-23 08:42:31 +0800

    add progress bar

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from datetime import datetime
 from key import API_TOKEN_KEY
+from tqdm import tqdm
 
 
 def upScaling(img):
@@ -8,11 +9,27 @@ def upScaling(img):
         "https://www.cutout.pro/api/v1/matting2?mattingType=18",
         files={"file": open(img, "rb")},
         headers={"APIKEY": API_TOKEN_KEY},
+        stream=True,  # Enable streaming response content
     )
+
+    # Get response content length in bytes
+    total_length = int(response.headers.get("content-length", 0))
+
+    # Create progress bar
+    progress_bar = tqdm(total=total_length, unit="iB", unit_scale=True)
+
     with open(
         "output/hasilnya-%s.png" % datetime.now().strftime("%Y-%m-%d_%H-%M-%S"), "wb"
     ) as out:
-        out.write(response.content)
+        for chunk in response.iter_content(chunk_size=1024):
+            if chunk:
+                # Update progress bar
+                progress_bar.update(len(chunk))
+
+                out.write(chunk)
+
+    # Close progress bar after download complete
+    progress_bar.close()
 
 
 upScaling(img="img/me.jpg")

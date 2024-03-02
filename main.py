890e161da222632b1d60afc74fdc5c6a3a0ef3f9
Halip26 <halipuddin.angko@gmail.com>
2024-03-02 14:30:26 +0800

    change req link & update code

--- main.py
+++ main.py
@@ -1,35 +1,43 @@
 import requests
-from datetime import datetime
 from key import API_TOKEN_KEY
+import sys
+from datetime import datetime
+
+# taqaddum, which means 'progress'.
 from tqdm import tqdm
 
 
 def upScaling(img):
     response = requests.post(
-        "https://www.cutout.pro/api/v1/matting2?mattingType=18",
+        "https://www.cutout.pro/api/v1/photoEnhance",
         files={"file": open(img, "rb")},
         headers={"APIKEY": API_TOKEN_KEY},
         stream=True,  # Enable streaming response content
     )
 
-    # Get response content length in bytes
+    # get respose content length in bytes
     total_length = int(response.headers.get("content-length", 0))
 
-    # Create progress bar
-    progress_bar = tqdm(total=total_length, unit="iB", unit_scale=True)
+    # create progress bar
+    progress_bar = tqdm(total=total_length, unit="iB")
 
-    with open(
-        "output/hasilnya-%s.png" % datetime.now().strftime("%Y-%m-%d_%H-%M-%S"), "wb"
-    ) as out:
-        for chunk in response.iter_content(chunk_size=1024):
-            if chunk:
-                # Update progress bar
-                progress_bar.update(len(chunk))
+    if response.status_code == requests.codes.ok:
+        with open(
+            "output/result-%s.png" % datetime.now().strftime("%Y-%m-%d_%H-%M-%S"), "wb"
+        ) as out:
+            for i in response.iter_content(chunk_size=1024):
+                if i:
+                    # update progress bar
+                    progress_bar.update(len(i))
 
-                out.write(chunk)
+                    out.write(i)
+    else:
+        print("Error:", response.status_code, response.text)
 
-    # Close progress bar after download complete
+    # close progress bar after download
     progress_bar.close()
 
 
-upScaling(img="img/me.jpg")
+image_path = sys.argv[1]
+
+upScaling(image_path)
